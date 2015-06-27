e3b3a5c52a9056be4991762efad459dfa684c47b
David Krmpotic <david.krmpotic@gmail.com>
2015-06-27 21:33:12 +0200

    silence warning

--- config.rb
+++ config.rb
@@ -1,6 +1,14 @@
 require 'yaml'
 
-Object.send(:remove_const, :Config)
+def silence_warnings(&block)
+  warn_level = $VERBOSE
+  $VERBOSE = nil
+  result = block.call
+  $VERBOSE = warn_level
+  result
+end
+
+silence_warnings { Object.send(:remove_const, :Config) if Object.const_defined?('Config') }
 
 class Config
 
